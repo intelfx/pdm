31a6ff49ed35926ad4a36bb03dc82e63d1e2f9fb
Ivan Shapovalov <intelfx@intelfx.name>
2025-04-13 09:41:32 +0700

    termui: make optional arguments to `echo()` keyword-only

--- src/pdm/termui.py
+++ src/pdm/termui.py
@@ -220,6 +220,7 @@ class UI:
     def echo(
         self,
         message: str | RichProtocol = "",
+        *,
         err: bool = False,
         verbosity: Verbosity = Verbosity.QUIET,
         **kwargs: Any,
